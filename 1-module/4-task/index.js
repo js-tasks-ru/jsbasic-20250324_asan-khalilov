ce0cf8a2ea05c4d72c5cf0eaa4f5e7d5a7fbd984
AsanKhalilov <khalilov2501@gmail.com>
2025-03-30 23:42:15 +0300

    The problem with the spam check is solved

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,10 @@
 function checkSpam(str) {
   // ваш код...
+  let normalizedStr = str.toLowerCase();
+
+  if (normalizedStr.includes("1xbet") || normalizedStr.includes("xxx")) {
+    return true;
+  }
+
+  return false;
 }
