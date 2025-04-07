9855476e52296990567cc83bae2ccddb3ec86c35
AsanKhalilov <khalilov2501@gmail.com>
2025-04-07 18:09:46 +0300

    Problem with finding min/max solved

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
   // ваш код...
+  const customArr = str
+    .split(' ')
+    .map(Number)
+    .filter(el => !isNaN(el))
+
+  return {
+    min: Math.min(...customArr),
+    max: Math.max(...customArr)
+  }
 }
